93550a4e2b7740ea73e298740fc3f310bf7b7fe0
Michel Osnaya <aranrisck@gmail.com>
2019-08-18 13:03:19 -0500

    Segundo Commit Domingo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import React from 'react'
 import { render } from 'react-dom'
 // import serviceWorker from './serviceWorker'
-import HolaMundo from './components/hola-mundo/HolaMundo'
+// Material 
+import Reboot from 'material-ui/Reboot'
+// Estilos
 import './index.css'
 
+// Componentes
+import Header from './components/common/header/Header'
+import Liga from './components/liga/Liga'
+import Calendario from './components/calendario/Calendario'
+
+// El codigo
+const Root = () => {
+    return (
+        <div>
+            <Reboot />
+            <Header />
+            <div className="contenedor">
+                <Liga />
+                <Calendario />
+            </div>
+        </div>
+    )
+}
 
 
 
